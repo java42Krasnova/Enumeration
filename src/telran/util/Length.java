0ed37d09aad42f7091d8f6c53c148f32a8e3b1a8
ikrasnova <i.bas@me.com>
2021-12-20 01:36:31 +0200

    HW#22

--- src/telran/util/Length.java
+++ src/telran/util/Length.java
@@ -1,51 +1,52 @@
 package telran.util;
 
-
 public class Length implements Comparable<Length> {
 	float amount;
 	LengthUnit unit;
-	
-public Length(float amount, LengthUnit unit) {
+
+	public Length(float amount, LengthUnit unit) {
 		this.amount = amount;
 		this.unit = unit;
 	}
 
-/**
- * equals two length objects according to LengthUnit
- * 10m == 10000mm
- */
+	/**
+	 * equals two length objects according to LengthUnit 10m == 10000mm
+	 */
 	@Override
-public boolean equals(Object obj) {
-	//TODO done!
-	return compareTo((Length) obj) == 0;
-		}
+	public boolean equals(Object obj) {
+		// TODO done!
+		return compareTo((Length) obj) == 0;
+	}
+
 	@Override
 	public int compareTo(Length o) {
-		//1m>1mm
-		//10000m>1km
+		// 1m>1mm
+		// 10000m>1km
 		// TODO !done
-		//return Float.compare(amount, o.convert(unit).amount);
+		// return Float.compare(amount, o.convert(unit).amount);
 		return (int) (amount - o.convert(unit).amount);
 	}
+
 	/**
 	 * 
 	 * @param unit
-	 * @return new Length object with a given LengthUnit 
-	 * convert(LengthUnit.M) returns Length in meters
+	 * @return new Length object with a given LengthUnit convert(LengthUnit.M)
+	 *         returns Length in meters
 	 */
-public	Length convert(LengthUnit unit) {
-	//TODO done
-	float lenght = (amount * this.unit.getValue())/ unit.value;
-	return new Length(lenght, unit);	
+	public Length convert(LengthUnit unit) {
+		// TODO done
+		float lenght = (amount * this.unit.getValue()) / unit.value;
+		return new Length(lenght, unit);
 	}
-/**
- * return string with amount and length unit pinned to amount with no space
- * Example 20M (string expression of length obj designed 20 meters)
- */
-@Override
-public String toString(){
-	//TODO DONE
-	return  String.format("%.2f", this.amount)+ this.unit.toString();
-}
-	
+
+	/**
+	 * return string with amount and length unit pinned to amount with no space
+	 * Example 20M (string expression of length obj designed 20 meters)
+	 */
+	@Override
+	public String toString() {
+		// TODO DONE
+		return String.format("%.2f", this.amount) + this.unit.toString();
+	}
+
 }
--- src/telran/util/LengthTest.java
+++ src/telran/util/LengthTest.java
@@ -17,7 +17,6 @@ class LengthTest {
 	Length l6_MM = new Length(10000, LengthUnit.MM);
 	Length l7_M = new Length(10, LengthUnit.M);
 
-
 	@BeforeEach
 	void setUp() throws Exception {
 	}
@@ -25,18 +24,19 @@ class LengthTest {
 	@Test
 	void testEqualsObject() {
 		// TODO done!
-		assertTrue(l1_CM.equals(l2_IN));//5in == 12,f cm
-		assertFalse(l2_IN.equals(l3_KM));//12,7 cm == 5 in
-		assertTrue(l4_FT.equals(new Length(1.2192f, LengthUnit.M)));//4ft == 1,2192m
+		assertTrue(l1_CM.equals(l2_IN));// 5in == 12,f cm
+		assertFalse(l2_IN.equals(l3_KM));// 12,7 cm == 5 in
+		assertTrue(l4_FT.equals(new Length(1.2192f, LengthUnit.M)));// 4ft == 1,2192m
 	}
 
 	@Test
 	void testCompareTo() {
 		// TODO done!
 		assertTrue(isGreather(l4_FT, l2_IN));// 1219.2 mm > 127mm
-		assertFalse(isGreather(l2_IN, l4_FT)); 
-		assertEquals(0, l1_CM.compareTo(l2_IN));//5in == 12,7 cm
+		assertFalse(isGreather(l2_IN, l4_FT));
+		assertEquals(0, l1_CM.compareTo(l2_IN));// 5in == 12,7 cm
 	}
+
 	private boolean isGreather(Length l1, Length l2) {
 		return l1.compareTo(l2) > 0;
 	}
@@ -44,24 +44,24 @@ class LengthTest {
 	@Test
 	void testConvert() {
 		// TODO done!
-		assertEquals(l1_CM, l2_IN.convert(LengthUnit.CM));//12.7 cm == 5in
-		assertEquals(l2_IN, l1_CM.convert(LengthUnit.IN));//5in==12.7 cm
-		assertEquals(l7_M, l6_MM.convert(LengthUnit.M));//10 m == 10000mm
+		assertEquals(l1_CM, l2_IN.convert(LengthUnit.CM));// 12.7 cm == 5in
+		assertEquals(l2_IN, l1_CM.convert(LengthUnit.IN));// 5in==12.7 cm
+		assertEquals(l7_M, l6_MM.convert(LengthUnit.M));// 10 m == 10000mm
 	}
 
 	@Test
 	void testToString() {
 		// TODO done!
 		assertEquals("4,00FT", l4_FT.toString());
-		System.out.println(l1_CM.toString());//rounded
+		System.out.println(l1_CM.toString());// rounded
 	}
 
 	@Test
 	void testBetween() {
 		// TODO done!
-		Length exp1 = new Length(1f, LengthUnit.M);//1 m -> 200 cm
+		Length exp1 = new Length(1f, LengthUnit.M);// 1 m -> 200 cm
 		assertEquals(exp1, LengthUnit.M.between(new Length(200f, LengthUnit.CM), new Length(1f, LengthUnit.M)));
-		Length exp2 = new Length(9000f, LengthUnit.CM); //10000m -> 1 km
+		Length exp2 = new Length(9000f, LengthUnit.CM); // 10000m -> 1 km
 		assertEquals(exp2.amount, LengthUnit.M.between(l5_M, l3_KM).amount, 0.01f);
 	}
 
